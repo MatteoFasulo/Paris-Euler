0c455d1ac35983deee71f56e1f3d2568c6adcf51
Matteo Fasulo <mat.fasulo@gmail.com>
2022-05-22 09:17:02 +0200

    Co-authored-by: Simone Flavio <smoxy@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,7 @@
+#TODO: Analisi nodes edges
+#   CCDF -> plot e commento
+#   riplottare tabella
+#   Analisi Top-Down
+#   Creazione modello matematico (vedere a quale modello la nostra network più si assomiglia)
+#   Ego network
+#   mappe html
